330f986cc21f63817a8d47d88dcebe377ef8fadf
zhuyong96 <252244282@qq.com>
2024-02-21 12:34:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -31,7 +31,7 @@ export default {
 			if (!upgradeHeader || upgradeHeader !== 'websocket') {
 				const url = new URL(request.url);
 				switch (url.pathname) {
-					case '/cf':
+					case '/cf/${userID_Path}':
 						return new Response(JSON.stringify(request.cf, null, 4), {
 							status: 200,
 							headers: {
